55031d563ace6c07c70fd5f409cbac76c04f73db
cheanz <cheanzcg@gmail.com>
2025-09-29 05:17:28 -0400

    Initial Commit

---
+++ birdy/birdyApp.swift
@@ -0,0 +1,17 @@
+//
+//  birdyApp.swift
+//  birdy
+//
+//  Created by Chenge Zhang on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct birdyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ birdyTests/birdyTests.swift
@@ -0,0 +1,17 @@
+//
+//  birdyTests.swift
+//  birdyTests
+//
+//  Created by Chenge Zhang on 9/29/25.
+//
+
+import Testing
+@testable import birdy
+
+struct birdyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
