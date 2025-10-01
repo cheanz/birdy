74b8bfe30f0323aefbc7744d29a23097ba6c68f7
cheanz <59786755+cheanz@users.noreply.github.com>
2025-10-01 13:50:41 -0400

    usr avatar

---
+++ birdy/ImagePicker.swift
@@ -0,0 +1,38 @@
+import SwiftUI
+import PhotosUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration(photoLibrary: .shared())
+        config.filter = .images
+        config.selectionLimit = 1
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator { Coordinator(self) }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: ImagePicker
+        init(_ parent: ImagePicker) { self.parent = parent }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+            guard let item = results.first else { return }
+            if item.itemProvider.canLoadObject(ofClass: UIImage.self) {
+                item.itemProvider.loadObject(ofClass: UIImage.self) { (obj, error) in
+                    DispatchQueue.main.async {
+                        if let ui = obj as? UIImage {
+                            self.parent.image = ui
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
